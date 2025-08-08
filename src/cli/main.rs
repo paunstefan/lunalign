53b74cfa8261e620bd279f9a46e5725818c0ed8d
paunstefan <paunstefan@protonmail.com>
2025-08-08 16:32:15 +0300

    SER decoding works. Needs refactor

--- src/cli/main.rs
+++ src/cli/main.rs
@@ -1,6 +1,10 @@
 use lunalign;
 
-fn main() -> Result<()> {
+use lunalign::decode;
+use std::path::Path;
+fn main() -> Result<(),()> {
+	let serfile = decode::SerFile::decode("testdata/16bit_mono.ser", Path::new("decoded"));
 
+	println!("{:?}", serfile);	
 	Ok(())
 }
