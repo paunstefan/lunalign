c4f1b595db2221b0f21dfb490f52fe4cc9e247bf
paunstefan <paunstefan@protonmail.com>
2025-08-13 11:00:17 +0300

    Started adding config

--- src/cli/main.rs
+++ src/cli/main.rs
@@ -1,13 +1,32 @@
 use lunalign;
 
+use anyhow::Result;
 use lunalign::decode::Decode;
 use lunalign::ser;
+use std::fs;
 use std::path::Path;
 
+use clap::{Args, Parser};
 
-fn main() -> Result<(),()> {
-	let serfile = ser::SerFile::decode_to_dir(Path::new("testdata/16bit_mono.ser"), Path::new("decoded"));
+#[derive(Parser, Debug)]
+#[clap(author = "Stefan Paun", version = "1.0", about, long_about = None)]
+struct Config {
+    #[arg(short, long)]
+    input: String,
+    // #[arg(short, long)]
+    // output: String,
+}
 
-	println!("{:?}", serfile);	
-	Ok(())
+fn main() -> Result<()> {
+    let config = Config::parse();
+
+    fs::create_dir_all("./process/input_fits");
+
+    let input_data = Path::new(&config.input);
+
+    if input_data.is_file() && input_data.extension().unwrap() == "ser" {
+        let serfile =
+            ser::SerFile::decode_to_dir(input_data, Path::new("./process/input_fits"));
+    }
+    Ok(())
 }
