bc06a66e39b628ad9a66de1bf7a5d0cbb3d7564f
paunstefan <paunstefan@protonmail.com>
2025-09-26 14:25:39 +0300

    Added test files

--- src/debayer.cpp
+++ src/debayer.cpp
@@ -1,8 +1,42 @@
+#include <filesystem>
+#include <iostream>
+#include <print>
 #include "debayer.hpp"
 #include "result.hpp"
 #include <librtprocess.h>
 
+#include <fitsio.h>
+
+namespace fs = std::filesystem;
+
+la_result debayer_fits(fitsfile *file);
+
 la_result run_debayer(std::unordered_map<std::string, std::string>& args)
 {
-    return la_result::Ok;
+    if (!args.contains("in"))
+    {
+        std::println("Error: Required argument 'in' for 'decode' not present!");
+        return la_result::Error;
+    }
+    fs::path input_file = args["in"];
+
+    fs::path output_dir = "debayered";
+    if (args.contains("out"))
+    {
+        output_dir = args["out"];
+    }
+    if (input_file.extension() != ".ser")
+    {
+        std::println(std::cerr, "Error: Only .ser files can be decoded.");
+        return la_result::Error;
+    }
+
+    la_result result = debayer_fits();
+
+    return result;
+}
+
+la_result debayer_fits(fitsfile *file)
+{
+
 }
